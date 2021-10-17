a42bd39ea01de84099e9f1a5ba159df5662697c9
Philip Aguilar Bremer <philip231001@googlemail.com>
2021-10-17 12:23:55 +0200

    Fix query

--- src/services/CourseAccessService.ts
+++ src/services/CourseAccessService.ts
@@ -13,13 +13,13 @@ class CourseAccessService {
 
     // returns total course access for the last seven days
     getTotalCourseAccess(courseId: number) {
-        return this.promisePool.query("SELECT COUNT(*) FROM `CourseAccess` WHERE accessed > DATE_SUB(NOW(),INTERVAL 7 DAYS)\
+        return this.promisePool.query("SELECT COUNT(*) FROM `CourseAccess` WHERE `accessed` > DATE_SUB(NOW(),INTERVAL 7 DAY)\
         AND `c_id` =?", [courseId]);
     }
 
     // returns total number of users who accessed the course during the last seven days
     getCourseAccessByUsers(courseId: number) {
-        return this.promisePool.query("SELECT COUNT(*) FROM `CourseUserRelation` WHERE visited > DATE_SUB(NOW(),INTERVAL 7 DAYS)\
+        return this.promisePool.query("SELECT COUNT(*) FROM `CourseUserRelation` WHERE visited > DATE_SUB(NOW(),INTERVAL 7 DAY)\
         AND `c_id` =?", [courseId]);
     }
 }
